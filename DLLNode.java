e8d52df9b1d3e546b7bec7c8abeb52c4050b896d
John Doe <johndoe@example.com>
2023-02-13 19:44:46 -0800

    first commit

---
+++ DLLNode.java
@@ -0,0 +1,11 @@
+public class DLLNode {
+  public Integer value;
+  public DLLNode previous;
+  public DLLNode next;
+
+  public DLLNode(Integer val) {
+    this.value = val;
+    this.previous = null;
+    this.next = null;
+  }
+}
